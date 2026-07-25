c10db3b3efc68fff8cd06ea026098cf60763e2c2
Evgeniy Mineev <e.mineev@crazypandagames.com>
2026-07-25 23:43:23 +0400

    saf support

--- Android.mk
+++ Android.mk
@@ -41,9 +41,11 @@ ifeq ($(SUPPORT_HARFBUZZ),true)
 endif
 
 ifeq ($(APP_OPTIM),debug)
-	LOCAL_LDLIBS := $(SDL_TTF_LOCAL_PATH)/../../../../../sdl2-compat//build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
+	LOCAL_LDLIBS += $(SDL_TTF_LOCAL_PATH)/../../../../../sdl2-compat/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
+	LOCAL_LDLIBS += $(SDL_TTF_LOCAL_PATH)/../../../../../SAFFAL/saffal/build/intermediates/merged_native_libs/debug/mergeDebugNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libsaffal.so
 else
-	LOCAL_LDLIBS := $(SDL_TTF_LOCAL_PATH)/../../../../../sdl2-compat//build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
+	LOCAL_LDLIBS += $(SDL_TTF_LOCAL_PATH)/../../../../../sdl2-compat/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libSDL2.so
+	LOCAL_LDLIBS += $(SDL_TTF_LOCAL_PATH)/../../../../../SAFFAL/saffal/build/intermediates/merged_native_libs/release/mergeReleaseNativeLibs/out/lib/$(TARGET_ARCH_ABI)/libsaffal.so
 endif
 
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/../../../../../sdl2-compat/src/main/jni/sdl2-compat/include
